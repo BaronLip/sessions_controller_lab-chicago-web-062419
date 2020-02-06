02d7a297aeb03bcb0927b416c485a6cabd04b91a
BaronLip <baronlip@gmail.com>
2020-02-06 12:03:58 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,16 @@ class SessionsController < ApplicationController
     end
 
     def create
-        # if Rail's session is created by assigning params[:name] to session[:name], redirect to home.
-        if session[:name] = params[:name]
+        
+        
+        # If params does not contain a value for [:name] and there is noting in it's place, request user to login by redirecting to login page. 
+        if params[:name]=== nil || params[:name].length === 0
+            redirect_to controller: 'sessions', action: 'new'
+            # redirect_to '/login' ## works just fine but test required line 11.
+        else session[:name] = params[:name]
+            # Otherwise create Rail's session is created by assigning params[:name] to session[:name], redirect to home.
             redirect_to '/'
-        elsif
-        # Otherwise, if params does not contain a value for [:name] and there is noting in it's place, request user to login. 
-            params[:name] == nil || params[:name].length == 0
-            redirect_to '/login'
         end
-            
     end
 
     def destroy
