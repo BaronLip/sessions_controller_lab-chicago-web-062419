7241d9a1b05d10f15593929c03b750063a8dca7e
BaronLip <baronlip@gmail.com>
2020-02-06 11:32:12 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
     end
 
     def create
+        # if Rail's session is created by assigning params[:name] to session[:name], redirect to home.
         if session[:name] = params[:name]
             redirect_to '/'
         elsif
+        # Otherwise, if params does not contain a value for [:name] and there is noting in it's place, request user to login. 
             params[:name] == nil || params[:name].length == 0
             redirect_to '/login'
         end
@@ -14,6 +16,9 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+        # Delete the key of :name in session, therefore "logging out". 
         session.delete :name
+        # Redirect to login page after deleting session[:name]
+        redirect_to '/login'
     end
 end
